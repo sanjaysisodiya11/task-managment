00eb331c22d2f8bbb86589db2b033d9997d3f0a3
Sanjay Sisodiya <sanjay@bitcot.com>
2026-01-20 17:02:09 +0530

    https crate middleware

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -9,17 +9,13 @@ use Illuminate\Http\Middleware\TrustProxies as Middleware;
 
 class TrustProxies extends Middleware
 {
-        /**
-     * The trusted proxies for this application.
-     *
-     * @var array|string|null
+    /**
+     * Trust all proxies (Railway, Nginx, Load Balancer)
      */
     protected $proxies = '*';
 
     /**
-     * The headers that should be used to detect proxies.
-     *
-     * @var int
+     * Use X-Forwarded-* headers
      */
     protected $headers =
         Request::HEADER_X_FORWARDED_FOR |
