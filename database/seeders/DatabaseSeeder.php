6b341d2c2e41a3bf5b15647338ce33fb25aba115
Sanjay Sisodiya <sanjay@bitcot.com>
2026-01-21 11:39:38 +0530

    create seeder for default two users

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,10 +16,22 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
+        User::factory()->create([
+            'name' => 'admin user',
+            'first_name' => 'admin',
+            'last_name' => 'user',
+            'email' => 'admin@example.com',
+            'password' => '12345678',
+            'role' => 'admin'
+        ]);
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'regular user',
+            'first_name' => 'regular',
+            'last_name' => 'user',
+            'email' => 'user@example.com',
+            'password' => '12345678',
+            'role' => 'employee'
         ]);
     }
 }
